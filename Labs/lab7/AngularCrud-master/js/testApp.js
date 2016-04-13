a9c32e575960881c891b27e259c7f18e8c2630ca
Kevin Kuo <kkuo12887@gmail.com>
2016-04-12 20:57:47 -0400

    Adding mods to make index.html render.

--- Labs/lab7/AngularCrud-master/js/testApp.js
+++ Labs/lab7/AngularCrud-master/js/testApp.js
@@ -10,17 +10,55 @@
 				$scope.heading = 'My Library';
 				
 				$scope.error = false;
-				$scope.books = [];
+				$scope.books = 
+				    [
+                        {
+                            "title": "The Princess Bride",
+                            "author": "William S. Goldman",
+                            "pageCount": 512,
+                            "starRating": 4.4
+                        },
+                        {
+                            "title": "Friday Night Lights",
+                            "author": "H. G. Bissinger",
+                            "pageCount": 432,
+                            "starRating": 3.7
+                        },
+                        {
+                            "title": "A Game of Thrones",
+                            "author": "George R. R. Martin",
+                            "pageCount": 807,
+                            "starRating": 4.8
+                        },
+                        {
+                            "title": "Stranger in a Strange Land",
+                            "author": "Robert A. Heinlein",
+                            "pageCount": 525,
+                            "starRating": 4.2
+                        },
+                        {
+                            "title": "Harry Potter and the Goblet of Fire",
+                            "author": "J. K. Rowling",
+                            "pageCount": 757,
+                            "starRating": 4.9
+                        },
+                        {
+                            "title": "Starship Troopers",
+                            "author": "Robert A. Heinlein",
+                            "pageCount": 432,
+                            "starRating": 4.9
+                        }
+				    ];
 
-				$http.get('/json/books.json').then(function (response) {
-					// Success Handling
-					$log.debug(response);
-					$scope.books = response.data.books;
-				}, function (response) {
-					// Error Handling
-					$log.debug(response);
-					$scope.error = true;
-				});
+				//$http.get('/json/books.json').then(function (response) {
+				//	// Success Handling
+				//	$log.debug(response);
+				//	$scope.books = response.data.books;
+				//}, function (response) {
+				//	// Error Handling
+				//	$log.debug(response);
+				//	$scope.error = true;
+				//});
 
 				$scope.edit = function (book) {
 					
